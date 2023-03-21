f40ccf63ec4a4c473a4fe9298113218d7a4fa7ba
vladimir <markov.pr@yandex.ru>
2023-03-21 18:30:32 +0300

    wrote 2 funcs() rand integer and rand float

---
+++ js/main.js
@@ -0,0 +1,26 @@
+//Генерация случайного целого числа из диапазона от a до b включительно
+
+function getRandomPositiveInteger ( a, b = 1 ) {
+  if (a === undefined) {
+    throw new Error ('Первый параметр должен быть число');
+  }
+  const min = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const max = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+getRandomPositiveInteger(0,1);
+
+//Генерация случайного числа с плавающей точкой из диапазона от a до b с n знаков после запятой
+
+function getRandomPositiveInteger ( a, b = 1 , n ) {
+  if (a === undefined) {
+    throw new Error ('Первый параметр должен быть число');
+  }
+  const min = Math.min(Math.abs(a), Math.abs(b));
+  const max = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (max - min + 1) + min;
+  return +result.toFixed(n);
+}
+
+console.log(getRandomPositiveInteger ( 0, 5 ,3 ));
